3c13a8629c4d0e49476675ab662e256281ebca79
Hannah Simon <147353617+hannahgsimon@users.noreply.github.com>
2024-09-22 01:37:45 -0400

    Update Neighbor.java

--- Neighbor.java
+++ Neighbor.java
@@ -1,15 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package graphs;
+package project_dijkstrasalgorithm;
 
 /**
  *
- * @author hgsim
+ * @author hannahgsimon
  */
-public class Neighbor {
-    
+
+public class Neighbor
+{
     private int vertex;
     private int cost;
     
